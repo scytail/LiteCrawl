961cff02d465cbedaf4684d95b08c7ecd2d68f9f
Ben Schwabe <bspymaster@gmail.com>
2022-04-11 16:40:09 -0500

    Added the ability to add a point value to interactables and set some defaults.

--- Assets/Scripts/Interactables/InteractableController.cs
+++ Assets/Scripts/Interactables/InteractableController.cs
@@ -2,6 +2,9 @@ using UnityEngine;
 
 public abstract class InteractableController : MonoBehaviour
 {
+    [SerializeField]
+    private int pointValue;
+
     private GameObject _gameController;
     protected GameObject GameManager { 
         get
